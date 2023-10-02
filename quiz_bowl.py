e1430a4d2fad0408967ff379009b5f2012d2f753
PrairieDingo <danjel.g.nygard.27@gmail.com>
2023-10-01 20:31:44 -0500

    Add repeat to missed answers and question limit

--- quiz_bowl.py
+++ quiz_bowl.py
@@ -21,30 +21,57 @@ def import_info():
     world_file.close()
 
 def ask_state_capitol_question():
+    global num_correct
     state = random.choice(list(state_capitols.keys()))
     print("What is the state capitol of " + state + "?")
     answer = input().lower().strip()
     if answer == state_capitols[state].lower():
         print('That is correct!')
+        num_correct += 1
     else:
         print("I'm sorry! The answer is " + state_capitols[state])
+        while answer != state_capitols[state].lower():
+            print('What is the state capitol of ' + state + '?')
+            answer = input().lower().strip()
 
 def ask_world_capitol_question():
+    global num_correct
     nation = random.choice(list(world_capitols.keys()))
     print("What is the capitol of " + nation + "?")
     answer = input().lower().strip()
     if answer == world_capitols[nation].lower():
         print('That is correct!')
+        num_correct += 1
     else:
         print("I'm sorry! The answer is " + world_capitols[nation])
+        while answer != world_capitols[nation].lower():
+            print('What is the capitol of ' + nation + '?')
+            answer = input().lower().strip()
 
+def run_quiz(q_number, categories):
+    for x in range(q_number):
+        asks = categories
+        fn = random.choice(asks)
+        fn()
+
+#Start the program
 import_info()
-
 while True:
-    asks = [ask_state_capitol_question, ask_world_capitol_question]
-    fn = random.choice(asks)
-    fn()
+    print('How many questions would you like to answer?')
+    q_number = 0
+    num_correct = 0
+    while(q_number == 0):
+        try:
+            q_number = int(input())
+            if (q_number==0):
+                print('Enter a number greater than zero.')
+        except:
+            print('Make sure to enter an integer greater than zero.')
 
+    categories = [ask_state_capitol_question, ask_world_capitol_question]
+    run_quiz(q_number, categories)
+
+    print('You answered ' + str(num_correct) + ' of ' + str(q_number) + ' questions correctly on the first try!\n')
     print('Try again? (y for yes)')
     if not input().lower().startswith('y'):
         break
